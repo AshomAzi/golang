8a9553a3b82deccb5cdc8b7aa0d57e25e82a9141
AshomAzi <ashomazibulus@gmail.com>
2026-05-15 15:15:37 +0100

    Add Contacts done

--- maps/3BasicPhonebook/main.go
+++ maps/3BasicPhonebook/main.go
@@ -1,9 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
-func PhoneBook() {
+func AddContact(name, number string, contacts map[string]string) map[string]string {
 
+	contacts[name] = number
+
+	return contacts
+
+}
+
+func TotalContacts(contacts map[string]string) int {
+	return len(contacts)
 }
 
 func main() {
@@ -14,4 +27,45 @@ func main() {
 	fmt.Println("4. Delete Contact")
 	fmt.Println("5. List all contacts")
 	fmt.Println("6. Exit")
+
+	contacts := make(map[string]string)
+	for {
+		fmt.Print("Chose any number from the options above: ")
+		option := bufio.NewScanner(os.Stdin)
+		option.Scan()
+		each := strings.Fields(option.Text())
+		if len(each) == 1 {
+			if each[0] == "1" {
+				fmt.Print("Input the contacts name: ")
+				name := bufio.NewScanner(os.Stdin)
+				name.Scan()
+				mName := strings.Fields(name.Text())
+				if len(mName) == 1 {
+					fmt.Print("Input the contacts number: ")
+					number := bufio.NewScanner(os.Stdin)
+					number.Scan()
+					num := strings.Fields(number.Text())
+					if len(num) == 1 {
+						AddContact(mName[0], num[0], contacts)
+					} else {
+						fmt.Println("Invalid Number!")
+					}
+				} else {
+					fmt.Println("Invalid name!")
+				}
+			} else if each[0] == "6" {
+				fmt.Println(contacts)
+				os.Exit(1)
+			} else {
+				fmt.Println("Invalid Option!")
+			}
+		} else {
+			fmt.Println("Invalid option!")
+		}
+	}
+
 }
+
+
+
+				
